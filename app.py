819cdb890c1042c74ef94f0f85998e2b45a49e39
Andre Kolber <andrekolber1@gmail.com>
2021-10-28 13:47:53 -0400

    modified app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
