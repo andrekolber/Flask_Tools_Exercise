88a37158f8e6141f536b40830d07f631c19d2ed7
Andre Kolber <andrekolber1@gmail.com>
2021-10-28 13:58:43 -0400

    moved responses list

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-responses = []
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "asdasd"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
 
 
 @app.route("/")
